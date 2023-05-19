379f0ca4ee8d354d7e9db4e68b2a2799ac3f2289
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-05-20 02:34:09 +0600

    Update toy information in the database using the provided ID and send the result as a response

--- index.js
+++ index.js
@@ -74,6 +74,30 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/addtoys/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await addToysCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.put('/addtoys/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const options = {upsert: true};
+            const updatedToys = req.body;
+            const toys = {
+                $set: {
+                    photo: updatedToys.photo,
+                    name: updatedToys.name,
+                    price: updatedToys.price,
+                    quantity: updatedToys.quantity
+                }
+            }
+            const result = await addToysCollection.updateOne(filter, toys, options);
+            res.send(result);
+        })
+
         app.delete('/addtoys/:id', async(req, res) => {
             const id = req.params.id;
             const query = {_id: new ObjectId(id)}
